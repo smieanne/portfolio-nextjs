e6f9713a84e897707cb08db45d22d9fd25ba15af
smieanne <mieannen.sugiyama@g-seed.co.jp>
2024-05-31 16:50:20 +0900

    Add アニメーション

--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,21 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import "../app/animations.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Hero() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000, // アニメーションの継続時間を設定
+    });
+  }, []);
+
   return (
-    <section className=" w-full lg:h-screen h-fit  relative   max-xs:mb-[40px]">
-      <div className="w-full h-full  flex items-center justify-between  max-md:flex-col max-md:gap-10 max-md:text-right ">
+    <section className="w-full lg:h-screen h-fit relative max-xs:mb-[40px]">
+      <div className="w-full h-full flex items-center justify-between max-md:flex-col max-md:gap-10 max-md:text-right">
         <div
           style={{
             backgroundImage: `url('/assets/top.jpg')`,
@@ -14,15 +23,26 @@ function Hero() {
             height: "100vh",
             backgroundSize: "cover",
             backgroundPosition: "center",
-
             zIndex: 10,
           }}
         >
-          <div className="flex flex-col gap-5 z-40 max-md:order-2  mt-60 ml-20 max-sm:mt-28">
+          <div className="flex flex-col gap-5 z-40 max-md:order-2 mt-60 ml-20 max-sm:mt-28">
             <div className="text-4xl lg:text-6xl text-white">
-              <h1 className="font-medium ">Welcome to</h1>
-              <h2 className="font-bold text-red-800">My Portfolio Site</h2>
-              <p className=" text-2xl font-bold max-sm:text-sm">
+              <h1
+                className="font-medium"
+                data-aos="fade-up"
+                data-aos-duration="2000"
+              >
+                Welcome to
+              </h1>
+              <h2 className="font-bold text-red-800" data-aos="fade-right">
+                My Portfolio Site
+              </h2>
+              <p
+                className="text-2xl font-bold max-sm:text-sm"
+                data-aos="fade-down"
+                data-aos-duration="2000"
+              >
                 Dreaming up and realize our dreams.
               </p>
 
@@ -39,8 +59,8 @@ function Hero() {
             </div>
           </div>
 
-          {/*animetioncss */}
-          <ul className="circles ">
+          {/* Animation CSS */}
+          <ul className="circles">
             <li></li>
             <li></li>
             <li></li>
